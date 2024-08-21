e21dee45d2642fc4d706f2be0be74cdfbd6afd49
vengatavaradhanp <77961420+vengatavaradhanp@users.noreply.github.com>
2024-08-21 17:51:34 +0530

    common component.

---
+++ src/components/Common/TAccordion.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Panel, PanelGroup } from "rsuite";
+interface TimerProps {
+  header?: string;
+  render?: any;
+  classname?: string;
+}
+const TAccordion: React.FC<TimerProps> = ({ header, render, classname }) => {
+  return (
+    <>
+      <PanelGroup accordion bordered className={classname}>
+        <Panel header={header} defaultExpanded>
+          {render}
+        </Panel>
+      </PanelGroup>
+    </>
+  );
+};
+
+export default TAccordion;
