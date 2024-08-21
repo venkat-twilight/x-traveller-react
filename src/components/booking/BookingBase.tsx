1b2d1317ff719b4300d08967a46d23f734d49f86
vengatavaradhanp <77961420+vengatavaradhanp@users.noreply.github.com>
2024-08-21 10:23:38 +0530

    Booking page.

---
+++ src/components/booking/BookingBase.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { List, Grid, Row, Col, Heading, Text, Stack, Panel } from "rsuite";
+import FareDetails from "@/src/components/Payment-component/FareDetails";
+import { FlightInfo } from "@/src/components/Flight-listing-card/FlightListBanner/FlightDetailsPopup";
+import FlightListingPage from "@/src/components/Flight-listing-card/FlightListingPage";
+import styles from "../../assets/styles/app-footer.module.css";
+
+export default function PaymentBase() {
+  return (
+    <Grid
+      style={{
+        padding: "20px 0px",
+      }}
+    >
+      <Row className="show-grid">
+        <Col xs={18}>
+          <Panel
+            className={styles.panel}
+            style={{
+              textWrap: "wrap",
+              border: "1px solid lightgrey",
+              lineHeight: "2",
+              marginLeft: "-20px",
+            }}
+          >
+            <FlightInfo />
+          </Panel>
+        </Col>
+        <Col xs={6}>
+          <FareDetails />
+        </Col>
+      </Row>
+    </Grid>
+  );
+}
